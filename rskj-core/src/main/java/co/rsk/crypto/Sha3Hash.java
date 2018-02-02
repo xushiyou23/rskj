73431f607441477114beaa71ea37c478e62f172d
Martín Coll <mc@rsk.co>
2018-02-02 14:34:03 -0300

    Use ByteUtil to clone bytes instead of SpongyCastle implementation.

--- rskj-core/src/main/java/co/rsk/crypto/Sha3Hash.java
+++ rskj-core/src/main/java/co/rsk/crypto/Sha3Hash.java
@@ -21,7 +21,7 @@ package co.rsk.crypto;
 import co.rsk.bitcoinj.core.Utils;
 import com.google.common.primitives.Ints;
 import org.ethereum.rpc.TypeConverter;
-import org.spongycastle.pqc.math.linearalgebra.ByteUtils;
+import org.ethereum.util.ByteUtil;
 
 import java.io.Serializable;
 import java.util.Arrays;
@@ -82,7 +82,7 @@ public class Sha3Hash implements Serializable, Comparable<Sha3Hash> {
      * Returns an identical Sha3Hash with a copy of the the internal byte array.
      */
     public Sha3Hash copy() {
-        return new Sha3Hash(ByteUtils.clone(bytes));
+        return new Sha3Hash(ByteUtil.cloneBytes(bytes));
     }
 
     @Override
